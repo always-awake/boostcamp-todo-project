79528ddea47889919e7c922136997fe5528bd01d
always-awake <leemirim9654@gmail.com>
2019-10-02 16:15:42 +0900

    [ADD][back] project 객체에서 응답에 필요한 정보를 담고있는 json 객체를 리턴하는 시리얼라이즈 함수 구현

---
+++ server/utils/serializers/projectSerializers.js
@@ -0,0 +1,11 @@
+const projectSerializer = (project, user) => {
+  return {
+    pk: project.pk,
+    user_pk: project.user_pk,
+    task_lists_order: project.task_lists_order,
+  }
+};
+
+module.exports = {
+  projectSerializer
+};
