1cb03d6c9d74755d7ed55135b32c061dfadbb71b
always-awake <leemirim9654@gmail.com>
2019-10-07 21:53:15 +0900

    [ IMPROVE ] 웹팩 설치 및 설정파일 작성
    why
    - SPA를 구현할 때 생성되는 다수의 파일을 효율적으로 import하여 데모 데스트와 배포하기 위해 웹팩을 사용한다.
    
    what
    - 웹팩 모듈을 dev 의존성 모듈러 설치
    - 웹팩을 구동하는데 필요한 다양한 의존 모듈 설치
    - 웹팩에서 사용하는 다양한 로더 설치
    - 웹팩에서 사용할 플러그인 설치
        - html-webpack-plugin

---
+++ web/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.bundle.html',
+      template: path.resolve(__dirname, './index.html'),
+    })
+  ]
+};
