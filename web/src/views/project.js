b0329e318946f1ff2088dbd375193fbc395ac0cf
always-awake <leemirim9654@gmail.com>
2019-10-11 03:32:07 +0900

    [ FEATURE ] ProjectView 구현 (#31)
    why
    - ProjectModel의 상태에 따라 다르게 동작하는 뷰 객체 필요
    
    what
    - 옵저버를 구독하는 객체
    - 옵저버에 등록된 특정 이벤트에 맞는 동작을 하는 객체
    - ProjectModel의 noti를 받는 객체

---
+++ web/src/views/project.js
@@ -0,0 +1,31 @@
+import { Project } from '../components/project/project';
+import { ProjectController } from '../controllers/projectController';
+import { Observer } from '../utils/Observer';
+
+class ProjectView {
+
+  constructor(projectPk, projectName, insertionTagId) {
+    this.projectName = projectName;
+    this.insertionTagId = insertionTagId;
+    this.observer = new Observer();
+    this.projectController = new ProjectController(this.observer, projectPk);
+    this.project = new Project(this.projectName, this.insertionTagId);
+  }
+
+  init() {
+    this.observer.addSubscriber({
+      eventName: 'updateProjectView',
+      handler: this.updateView,
+      target: this
+    });
+    this.projectController.init();
+  }
+
+  updateView(responseData) {
+    this.target.project.init(responseData);
+  }
+}
+
+export {
+  ProjectView
+}
