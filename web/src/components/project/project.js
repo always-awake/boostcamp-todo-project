8f00fab1ca661ca75a02e8bd0c332cfb3b60ca79
always-awake <leemirim9654@gmail.com>
2019-10-11 03:37:54 +0900

    [ FEATURE ] Project 컴포넌트 객체 구현 (#32)
    why
    - 프로젝트 뷰에서 적절한 프로젝트 데이터가 포함된 컴포넌트를 렌더링하기 위함
    
    what
    - 프로젝트 데이터를 받아 적절한 HTML 코드를 렌더링함
    - 프로젝트 컴포넌트의 하위 컴포넌트인 TaskList 컴포넌트 생성

---
+++ web/src/components/project/project.js
@@ -0,0 +1,39 @@
+import { TaskList } from '../project/taskList';
+import { $ } from '../../utils/utils';
+
+class Project {
+
+  constructor(projectName, insertionTagId) {
+    this.projectName = projectName;
+    this.insertionTagId = insertionTagId;
+    this.taskListDivList = null;
+  }
+
+  init(projectData) {
+    this.createTaskListHTML(projectData);
+    this.render();
+  }
+
+  render() {
+    const insertionSection = $(`main#${this.insertionTagId}`);
+    insertionSection.innerHTML = `
+    <div id="projectTitle">${this.projectName}</div>
+    <div id="project">
+      ${this.taskListDivList}
+    </div>
+    `
+  }
+
+  createTaskListHTML(projectData) {
+    this.taskListDivList = Object.keys(projectData.task_list).reduce((acc, key ) => {
+      const tasks = projectData.task_list[key];
+      const taskList = new TaskList(tasks);
+      taskList.init();
+      return acc += taskList.render();
+    }, '');
+  }
+}
+
+export {
+  Project
+}
