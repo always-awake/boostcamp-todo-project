3c7d79270a9fe1804a8c728a6b3573e061607248
always-awake <leemirim9654@gmail.com>
2019-10-11 03:06:05 +0900

    [ REFACTORING ] index.js 함수 코드 리팩토링
    why
    - main함수 내 개행이 불규칙하게 되어 있어 수정이 필요
    
    what
    - 탭(스페이스 4칸)으로 되어 있었던 개행 부분을 스페이스 2칸으로 변경

--- web/src/index.js
+++ web/src/index.js
@@ -6,18 +6,16 @@ const main = () => {
   fetch(LOGIN_URL, {
     method: 'GET',
     credentials: 'include',
-  })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.is_login) {
-          const projectList = new ProjectListView('main');
-          projectList.render();
-        } else {
-          const loginView = new LoginView('main');
-          loginView.render();
-        }
-      })
-      .catch(err => console.error('Error: ', err));
+  }).then((res) => res.json())
+    .then((res) => {
+      if (res.is_login) {
+        const projectListView = new ProjectListView('main');
+        projectListView.render();
+      } else {
+        const loginView = new LoginView('main');
+        loginView.render();
+      }
+    }).catch(err => console.error('Error: ', err));
 };
 
 window.onload = () => {
