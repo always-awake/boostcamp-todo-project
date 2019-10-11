ddb280f5f47513aa160deb6be614de6c901290fa
always-awake <leemirim9654@gmail.com>
2019-10-11 10:52:46 +0900

    [ CHORE ] 유틸의 URL 파일에 코드 종류에 대한 주석추가

--- web/src/utils/URL.js
+++ web/src/utils/URL.js
@@ -1,9 +1,11 @@
 // domain
 const DOMAIN = 'http://localhost:3000';
 
-// api server url
+// api server url (const)
 const LOGIN_URL = `${DOMAIN}/users/login`;
 const PROJECT_LIST_URL = `${DOMAIN}/projects`;
+
+// api server url (func)
 const PROJECT_URL = (projectPk) => {
   return `${DOMAIN}/projects/${projectPk}`
 };
